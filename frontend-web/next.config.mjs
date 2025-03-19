acadeb5902bff8a799e07788c9002c80d2fb92ff
erfan dwi prasetya <erfan.dwi.prasetya@tonjoo.com>
2025-03-19 16:30:42 +0700

    update domain

--- frontend-web/next.config.mjs
+++ frontend-web/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     formats: ["image/avif", "image/webp"],
-    domains: ['img.youtube.com', 'placehold.co', 'localhost', 'admin.tap.tongkolspace.com', '127.0.0.1','backend'],
+    domains: ['img.youtube.com', 'placehold.co', 'localhost', 'admin.tap.tongkolspace.com', '127.0.0.1','backend','admin.rfan.my.id'],
     remotePatterns: [
       {
         protocol: process.env.FRONTEND_CMS_SERVER_API_PROTOCOL || 'http',
